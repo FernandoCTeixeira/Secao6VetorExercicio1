ba6af396885f1e539d0ae7c4a47b58da02470f92
Fernando C. Teixeira <fct0316@gmail.com>
2023-05-12 19:34:01 -0300

    Adicionar arquivos de projeto.

---
+++ Secao6VetorExercicio1/Estudante.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio1 {
+    internal class Estudante {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+
+
+        public Estudante() { 
+        }
+
+        public Estudante(string nome, string email) {
+            this.Nome = nome;
+            this.Email = email;
+        }
+
+
+        public override string ToString() {
+            return Nome
+                + ", "
+                + Email;
+        }
+    }
+}
